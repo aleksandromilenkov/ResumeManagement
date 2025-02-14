f5086a426352496c65b605c01556ee93ba446367
Aleksandro <aleksandro.macedonia@gmail.com>
2025-02-14 19:09:12 +0100

    adding dark mode and light mode toggle buttons from material ui in the navbar

--- ResumeManagementClient/src/components/Navbar/Navbar.tsx
+++ ResumeManagementClient/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
+import { Menu, LightMode, DarkMode } from "@mui/icons-material";
+import { ToggleButton } from "@mui/material";
+import { useTheme } from "../../context/themeContext";
 type Props = {};
 const links = [
   {
@@ -21,6 +24,7 @@ const links = [
   },
 ];
 const Navbar = (props: Props) => {
+  const {darkMode, toggleDarkMode} = useTheme();
   return (
     <div className="navbar">
       <div className="brand">
@@ -35,8 +39,19 @@ const Navbar = (props: Props) => {
           ))}
         </ul>
       </div>
-      <div className="hamburger"></div>
-      <div className="toggle"></div>
+      <div className="hamburger">
+          <Menu />
+      </div>
+      <div className="toggle">
+        <ToggleButton
+          value="check"
+          selected={darkMode}
+          onChange={toggleDarkMode}
+          style={{ color: darkMode ? "white" : "black" }}
+        >
+          {darkMode ? <LightMode/> : <DarkMode/>}
+        </ToggleButton>
+      </div>
     </div>
   );
 };
